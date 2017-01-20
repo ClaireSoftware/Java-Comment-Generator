736564cef58d4f92094866002ff349cfbcb888bc
ClaireSoftware <gameboy193@gmail.com>
2017-01-20 08:59:28 -0500

    Update ComGen.java

--- ComGen.java
+++ ComGen.java
@@ -2,38 +2,51 @@
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;  // Clipboard
 import java.awt.datatransfer.StringSelection; // String Selector
-import java.util.Scanner;
+import java.util.Scanner; //Keyboard Input
+import javax.swing.JOptionPane; //Options
 import java.util.Date; //Automatic date functions
 import java.text.SimpleDateFormat; //* Date function
+/*This is a modified version of my original ComGen program, which also generates the opening lines of a program.
+Sadly, this version does not indent the Lines properly. */
 
-
-public class ComGen
+public class ComGenExtendedPane
 {
 	public static void main(String[] args)
 	{
+		Boolean Check1, Check2, Check3;
 
-		String Name;
+		String name;
+		String entry;
+		String decisionString;
+		char decision;
 		//The name is assumed to be constant, if you want to use this for someone other than me, you would have to change the name
 		final String MyName= "Claire Software";
-		String Date;  //If the Date function does not work for you, uncomment all of the lines that have an * next to them
+		/*String Date; */ //If the Date function does not work for you, uncomment all of the lines that have an * next to them
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
 		Clipboard clipboard = toolkit.getSystemClipboard();
 		Scanner input= new Scanner(System.in);
 		Date dNow = new Date( ); //* Automatic Date Function
 		SimpleDateFormat ft = //*Automatic Date Function
       new SimpleDateFormat ("MM/dd/YY "); //* Automatic Date Function*
-		System.out.print("input the name of the project : ");
-		Name= input.next();
+      do
+      {
+		entry=JOptionPane.showInputDialog("input the name of the project : ");
+		name= entry;
 		/*System.out.print("input the date(DD/MM/YY) : ");
 		Date= input.next();*/
 		//You would change the below line if you want a different type of comment.
-		String Comment="/* Programmer: "+MyName+" | Program: "+Name +"\n | Programmed on " +ft.format(dNow)+" */"; // If you want to use manual dates,replace dNow with date
+			String Comment="/* Programmer: "+MyName+" | Program: "+name +"\n | Programmed on " +ft.format(dNow)+" */"+"\npublic class "
+		+name +"\n{"+"\n public static void main(String [] args)"+"\n{";// If you want to use manual dates,replace dNow with date
+	      //The above line generates a comment in the format /* Yourname | Program: ProgramName | Programmed on: Date and then generates the opening lines of the program
+	      
 		StringSelection strSel = new StringSelection(Comment);
 		clipboard.setContents(strSel, null);
-		System.out.println(Comment);
-		Signature.SignatureFlashy();
-		System.exit(0);
+		JOptionPane.showMessageDialog(null,Comment);
+		decisionString=JOptionPane.showInputDialog("do you want to do another program? (y/n) ");
+		decision=(char) decisionString.charAt(0);
 
-
+	}
+	while(decision=='y'||decision=='Y');
+	SignatureDialog.SignatureFlashy();
 	}
 }
